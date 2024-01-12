b25b68de3f6010bc77107750c3b9e744cea16233
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-01-12 12:04:34 +0530

    moved files to Exp1 folder

--- Exp1/Exp1i.py
+++ Exp1/Exp1i.py
@@ -2,4 +2,6 @@
 # Write a program that calculates and displays the value of a light-year. Write a program that takes as input a number of kilometers and prints the corresponding number of nautical miles. Use the following approximations:
 # • A kilometer represents 1/10,000 of the distance between the North Pole and the equator.
 # • There are 90 degrees, containing 60 minutes of arc each, between the North Pole and the equator.
+# • A nautical mile is 1 minute of an arc.
+
 
