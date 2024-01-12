75ae30a8f06cf763cee5095c2d0338a54dde6e48
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-01-12 11:22:22 +0530

    added files

--- Exp1e.py
+++ Exp1e.py
@@ -1,9 +1,6 @@
-# Get mass and velocity from user
 mass = float(input("Enter the mass of the object (in kg): "))
 velocity = float(input("Enter the velocity of the object (in m/s): "))
 
-# Calculate momentum
 momentum = mass * velocity
 
-# Output the result
 print("The momentum of the object is:", momentum, "kg/s")
