f15c2f0cd9500c3591bfb8d289eb7b4565dfdfa0
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-01-12 11:38:24 +0530

    updated 1c.py

--- Exp1c.py
+++ Exp1c.py
@@ -1,6 +1,17 @@
 # Five Star Retro Video rents VHS tapes and DVDs to the same connoisseurs who like to buy LP record albums. The store rents new videos for $3.00 a night, and oldies for $2.00 a night. Write a program that the clerks at Five Star Retro Video can use to calculate the total charge for a customer’s video rentals. The program should prompt the user for the number of each type of video and output the total
 
-new_videos = int(input("Enter the number of new videos rented: "))
-old_videos = int(input("Enter the number of old videos rented: "))
-total_charge = (new_videos * 3.00) + (old_videos * 2.00)
-print("Total charge: $", total_charge)
+print("Welcome to Five Star Retro Video")
+print("New videos: $3.00 each")
+print("Old videos: $2.00 each")
+print("Do you want old videos or new videos?")
+switch = input("Enter 'n' for new videos and 'o' for old videos: ")
+if switch == 'n':
+    new_videos = int(input("Enter the number of new videos rented: "))
+    total_charge = new_videos * 3.00
+    print("Total charge: $", total_charge)
+elif switch == 'o':
+    old_videos = int(input("Enter the number of old videos rented: "))
+    total_charge = old_videos * 2.00
+    print("Total charge: $", total_charge)
+else:
+    print("Invalid input")
