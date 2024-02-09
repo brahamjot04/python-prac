3ddc35e6f03f39e04ebfa9cfb337deae49bee358
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2024-02-09 11:18:59 +0530

    Update project settings and dependencies

---
+++ Exp2/Exp3e.py
@@ -0,0 +1,2 @@
+# The German mathematician Gottfried Leibniz developed the following method to approximate the value of n: n/4 = 1-1/3 + 1/5-1/7 + ... Write a program that allows the user to specify the number of iterations use in this             approximation and that displays the resulting value.
+
